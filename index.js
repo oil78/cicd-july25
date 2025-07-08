b1146644df794c4a87c50f17e5ecb20ad16bf479
Matt Hoyle <matt_h78@hotmail.com>
2025-07-08 09:42:27 +0000

    Trigger CI/CD

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require('http');
 const PORT = process.env.PORT || 8080;
 http.createServer((req, res) => {
   res.end('Hello from CI/CD in GCP!');
-}).listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+}).listen(PORT, () => console.log(`Server running on port ${PORT}`)); // test change
